9fc371bf52e18dba2d7628b14c5f4f75ec4b981c
cyril VICENTINI <cyrilvicentini@MacBook-Pro-de-cyril.local>
2019-10-28 12:57:47 +0100

    oui

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AuthService, User} from '../auth.service';
 import {Observable, of} from 'rxjs';
 import {catchError, distinctUntilChanged, map} from 'rxjs/operators';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-top-bar',
@@ -9,7 +10,8 @@ import {catchError, distinctUntilChanged, map} from 'rxjs/operators';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+              private router: Router) { }
 
   user;
 
